55ee0778a7616d7036f97df9f869b234f532c534
AiSaurabhpatil <aientrepreneur15@gmail.com>
2023-07-24 09:38:16 +0530

    data transformation funtionality added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from text_summarizer.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from text_summarizer.logging import logger 
 from text_summarizer.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
+from text_summarizer.pipelines.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -25,3 +26,14 @@ except Exception as e:
     logger.exception(e)
     raise e 
 
+
+STAGE_NAME = 'Data Tranfomation stage'
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_validation = DataTransformationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e 
